5a13d7eb7776736831f7abd94ee6a8dee7fd3063
talhawkk <talhawkk@gmail.com>
2025-06-22 20:40:13 +0500

    final

--- xthive/xthive/settings.py
+++ xthive/xthive/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',  # ✅ Add this here
+    'django.contrib.sessions.middleware.SessionMiddleware',  
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
